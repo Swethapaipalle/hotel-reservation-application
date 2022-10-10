4038402c9cbf1c46ce8b5126e8a354069a85765f
Swethapaipalle <swethapaipalle87@gmail.com>
2022-10-10 12:46:39 -0400

    validate the form

--- src/components/detail-page-container/DetailPage.jsx
+++ src/components/detail-page-container/DetailPage.jsx
@@ -5,19 +5,40 @@ import dayjs from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { InputLabel, Select, TextField, MenuItem, Checkbox, Switch } from '@mui/material';
+import { InputLabel, ListItemIcon, ListItemText, Select, TextField, MenuItem, Checkbox, Switch } from '@mui/material';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Tags from '../tags-container/Tags';
 import { ReservationContext } from "../../useContext/context";
+import { MenuProps, options } from './../../utils/utils';
 
 export default function DetailPage({ data, marginBotton, width, variant }) {
   console.log("data", data)
   const [arrival, setArrival] = React.useState(dayjs(data?.stay?.arrivalDate));
   const [departure, setDeparture] = React.useState(dayjs(data?.stay?.departureDate));
   const [roomSize, setRoomSize] = React.useState(data?.room?.roomSize);
+  const [roomQuantity, setRoomQuantity] = React.useState(data?.room?.roomQuantity);
+  const [firstName, setFirstName] = React.useState(data?.firstName);
+  const [lastName, setLastName] = React.useState(data?.lastName);
+  const [email, setEmailName] = React.useState(data?.email);
+  const [phone, setPhone] = React.useState(data?.phone);
+  const [streetName, setStreetName] = React.useState(data?.addressStreet?.streetName);
+  const [streetNumber, setStreetNumber] = React.useState(data?.addressStreet?.streetNumber);
+  const [zip, setZip] = React.useState(data?.addressLocation?.zipCode);
+  const [stateName, setStateName] = React.useState(data?.addressLocation?.state);
+  const [city, setCity] = React.useState(data?.addressLocation?.city);
   const [state, dispatch] = useContext(ReservationContext);
+  const [extras, setExtras] = React.useState(data?.extras);
+  const [note, setNote] = React.useState(data?.note);
+  const [reminder, setReminder] = React.useState(data?.reminder);
+  const [newsletter, setNewsletter] = React.useState(data?.newsletter);
+  const [confirm, setConfirm] = React.useState(data?.confirm);
+  const [tags, setTags] = React.useState(data?.tags);
+
+  const isAllSelected =
+    options.length > 0 && extras.length === options.length;
+
   const { reservations, noResults } = state
   console.log("reservations", reservations)
   const handleSelect = (event) => {
@@ -29,9 +50,27 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
   const departureHandleChange = (newValue) => {
     setDeparture(newValue);
   };
+  const onChangeRoomQuantity = (newValue) => {
+    setRoomQuantity(newValue)
+  };
+
+  const onChangeFirstName = (e) => {
+    setFirstName(e.target.value)
+  };
+
   const label1 = { inputProps: { 'aria-label': 'Send me a remainder' } };
   const label2 = { inputProps: { 'aria-label': 'Subscribe to newsletter' } };
   const label = { inputProps: { 'aria-label': 'Checkbox' } };
+
+  const handleExtrasChange = (event) => {
+    const value = event.target.value;
+    if (value[value.length - 1] === "all") {
+      setExtras(extras.length === options.length ? [] : options);
+      return;
+    }
+    setExtras(value);
+  };
+
   return (
     <Grid container>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -59,7 +98,7 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
           <InputLabel id="demo-simple-select-label">Room Size</InputLabel>
           <Select
             id="demo-simple-select"
-            value={roomSize||''}
+            defaultValue={roomSize || ''}
             label="Room Size"
             variant={variant}
             onChange={handleSelect}
@@ -68,22 +107,24 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
               value={"business-suite"}
             >
               Business-suite
-            </MenuItem> <MenuItem
-              value={"Studio"}
+            </MenuItem>
+            <MenuItem
+              value={"presidential-suite"}
             >
-              Studio
+              Presidential-suite
             </MenuItem>
           </Select>
           <InputLabel id="demo-simple-select-label">Choose a room type</InputLabel>
         </Grid>
         <Grid item xs={8} md={8} lg={8}>
-          <TextField type="text"
+          <TextField type="number"
             fullwidth
             label='Room quantity'
             aria-label="Search icons"
             aria-describedby="basic-addon2"
             variant="standard"
-          // onChange={onChange}
+            defaultValue={roomQuantity}
+          // onChange={onChangeRoomQuantity}
           />
           <InputLabel id="demo-simple-select-label">Maximum: 5</InputLabel>
         </Grid>
@@ -94,7 +135,8 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
             aria-label="Search icons"
             aria-describedby="basic-addon2"
             variant="standard"
-          // onChange={onChange}
+            defaultValue={firstName}
+            onChange={onChangeFirstName}
           />
           <InputLabel id="demo-simple-select-label">2/5</InputLabel>
         </Grid>
@@ -105,6 +147,7 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
             aria-label="Search icons"
             aria-describedby="basic-addon2"
             variant={variant}
+            defaultValue={lastName}
           // onChange={onChange}
           />
           <InputLabel id="demo-simple-select-label">3/50</InputLabel>
@@ -116,6 +159,7 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
             aria-label="Search icons"
             aria-describedby="basic-addon2"
             variant={variant}
+            defaultValue={email}
           // onChange={onChange}
           />
         </Grid>
@@ -126,6 +170,7 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
             aria-label="Search icons"
             aria-describedby="basic-addon2"
             variant={variant}
+            defaultValue={phone}
           // onChange={onChange}
           />
           <InputLabel id="demo-simple-select-label">Add your country code first</InputLabel>
@@ -137,6 +182,7 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
             aria-label="Search icons"
             aria-describedby="basic-addon2"
             variant={variant}
+            defaultValue={streetName}
           // onChange={onChange}
           />
         </Grid>
@@ -147,6 +193,7 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
             aria-label="Search icons"
             aria-describedby="basic-addon2"
             variant={variant}
+            defaultValue={streetNumber}
           // onChange={onChange}
           />
         </Grid>
@@ -157,6 +204,7 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
             aria-label="Search icons"
             aria-describedby="basic-addon2"
             variant={variant}
+            defaultValue={zip}
           // onChange={onChange}
           />
         </Grid>
@@ -167,6 +215,7 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
             aria-label="Search icons"
             aria-describedby="basic-addon2"
             variant={variant}
+            defaultValue={stateName}
           // onChange={onChange}
           />
           <InputLabel id="demo-simple-select-label">AutoComplete</InputLabel>
@@ -178,6 +227,7 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
             aria-label="Search icons"
             aria-describedby="basic-addon2"
             variant={variant}
+            defaultValue={city}
           // onChange={onChange}
           />
           <InputLabel id="demo-simple-select-label">Maximum: 5</InputLabel>
@@ -185,25 +235,59 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
         <Grid item xs={12} md={12} lg={12}>
           <InputLabel id="demo-simple-select-label">Extras</InputLabel>
           <Select
-            id="demo-simple-select"
-            value={"extras"}
-            label="Extras"
-            variant={variant}
-            onChange={handleSelect}
-            sx={{ width: 200, }}>
+            labelId="mutiple-select-label"
+            multiple
+            value={extras}
+            onChange={handleExtrasChange}
+            renderValue={(selected) => selected.join(", ")}
+            MenuProps={MenuProps}
+          >
+            <MenuItem
+              value="all"
+              classes={{
+                root: isAllSelected ? {
+                  backgroundColor: "rgba(0, 0, 0, 0.08)",
+                  "&:hover": {
+                    backgroundColor: "rgba(0, 0, 0, 0.08)"
+                  }
+                } : ""
+              }}
+            >
+              <ListItemIcon>
+                <Checkbox
+                  classes={{ indeterminate: { color: "#f50057" } }}
+                  checked={isAllSelected}
+                  indeterminate={
+                    extras.length > 0 && extras.length < options.length
+                  }
+                />
+              </ListItemIcon>
+              <ListItemText
+                classes={{ primary: {  fontWeight: 500 } }}
+                primary="Select All"
+              />
+            </MenuItem>
+            {options.map((option) => (
+              <MenuItem key={option} value={option}>
+                <ListItemIcon>
+                  <Checkbox checked={extras.indexOf(option) > -1} />
+                </ListItemIcon>
+                <ListItemText primary={option} />
+              </MenuItem>
+            ))}
           </Select>
         </Grid>
         <Grid item xs={6} md={6} lg={6}>
           <RadioGroup
             aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="female"
+            defaultValue="cc"
             name="radio-buttons-group"
             row
           >
-            <FormControlLabel value="female" control={<Radio />} label="Credit Card" />
-            <FormControlLabel value="male" control={<Radio />} label="Paypal" />
-            <FormControlLabel value="other" control={<Radio />} label="Cash" />
-            <FormControlLabel value="other" control={<Radio />} label="Bitcoin" />
+            <FormControlLabel value="cc" control={<Radio />} label="Credit Card" />
+            <FormControlLabel value="paypal" control={<Radio />} label="Paypal" />
+            <FormControlLabel value="cash" control={<Radio />} label="Cash" />
+            <FormControlLabel value="bitcoin" control={<Radio />} label="Bitcoin" />
           </RadioGroup>
         </Grid>
         <Grid item xs={12} md={12} lg={12}>
@@ -214,23 +298,24 @@ export default function DetailPage({ data, marginBotton, width, variant }) {
             aria-label="Search icons"
             aria-describedby="basic-addon2"
             variant={variant}
-          // onChange={onChange}
+            defaultValue={note}
+            // onChange={onChange}
           />
         </Grid>
         <Grid item xs={12} md={12} lg={12}>
           <InputLabel id="demo-simple-select-label">Tags</InputLabel>
-          <Tags />
+          <Tags data={tags}/>
         </Grid>
         <Grid item xs={12} md={12} lg={12}>
-          <Switch color="secondary" {...label1} defaultChecked />
+          <Switch color="secondary" {...label1} checked={reminder} />
           <span>Send me a remainder</span>
         </Grid>
         <Grid item xs={12} md={12} lg={12}>
-          <Switch color="secondary" {...label2} defaultChecked />
+          <Switch color="secondary" {...label2} checked={newsletter} />
           <span>Subscribe to newsletter</span>
         </Grid>
         <Grid item xs={12} md={12} lg={12}>
-          <Checkbox {...label} />
+          <Checkbox {...label} checked={confirm}/>
           <span>I confirm the information given above</span>
         </Grid>
       </LocalizationProvider>
---
+++ src/utils/utils.js
@@ -0,0 +1,31 @@
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250
+        }
+    },
+    getContentAnchorEl: null,
+    anchorOrigin: {
+        vertical: "bottom",
+        horizontal: "center"
+    },
+    transformOrigin: {
+        vertical: "top",
+        horizontal: "center"
+    },
+    variant: "menu"
+};
+
+const options = [
+    "extraBreakfast",
+    "extraTV",
+    "extraWiFi",
+    "extraParking",
+    "extraBalcony"
+];
+
+export { MenuProps, options };
